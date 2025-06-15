1a58d9cfe5652af49a270d1bf5afc3891df2f1b3
Shreeyanshi Gupta <158245215+shreeyanshi123@users.noreply.github.com>
2025-06-15 16:48:45 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom';
 import AuthLayout from './components/auth/layout'
 import AuthLogin from './pages/auth/login'
 import AuthRegister from './pages/auth/register'
